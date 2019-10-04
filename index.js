b8a1c717903f77a2cdc4fbd294a32c35932c4b86
PuneetK <puneet1103@hotmail.com>
2019-10-04 11:39:07 +0400

    Updating code to match new variables

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const functions = require('./functions');
-
-functions:functions.SendCoupon()
+let taskObject = require('./functions');
+console.log("---Flatten Array -----");
+let test = taskObject.flatArray([[1,2,[3]],4])
+console.log(test);
+console.log("---Show Coupon -----");
+taskObject.sendCoupon()
